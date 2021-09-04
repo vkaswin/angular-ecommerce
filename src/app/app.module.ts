1765b43d806833283c650d2c84a29e59442d9d17
vkaswin <vkaswin1998@gmail.com>
2021-09-04 22:02:19 +0530

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,29 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { HeaderComponent } from './component/header/header.component';
+import { FooterComponent } from './component/footer/footer.component';
+import { CartComponent } from './pages/cart/cart.component';
+import { ProductsComponent } from './pages/products/products.component';
+import { ProductDetailComponent } from './pages/product-detail/product-detail.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
+import { BaseLayoutComponent } from './layout/base-layout/base-layout.component';
+import { SearchComponent } from './component/header/search/search.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    FooterComponent,
+    CartComponent,
+    ProductsComponent,
+    ProductDetailComponent,
+    NotFoundComponent,
+    BaseLayoutComponent,
+    SearchComponent,
   ],
   imports: [
     BrowserModule,
